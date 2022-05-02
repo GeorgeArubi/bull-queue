26d6e8acde4d2d8ef462c9265991fe4bb468c139
DOGHOUSETV <arubigeorge0@gmail.com>
2022-05-02 09:43:24 -0400

    Configure bull-board for express

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-const app = express();
+import { ExpressAdapter } from '@bull-board/express';
 
+const serverAdapter = new ExpressAdapter();
+
+const app = express();
 app.use(bodyParser.json());
+
+serverAdapter.setBasePath('/admin/queues');
+app.use('/admin/queues', serverAdapter.getRouter());
 
 app.post('/send-email', async (req, res) => {
     res.send({ status: 'ok' });
